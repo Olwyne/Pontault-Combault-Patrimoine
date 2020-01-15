63828e0ed1a4d8589dbc6208c1e99ed66a9956fa
Sophie <sophbyr@gmail.com>
2020-01-15 15:20:36 +0100

    [+] Update specific data and delete

--- src/view/test.vue
+++ src/view/test.vue
@@ -1,13 +1,13 @@
 <template>
     <div>
       <!-- <button @click="read">Lire</button>
-      <button @click="update">Update</button>
-      <button @click="deletel">Delete</button> -->
+      <button @click="update">Update</button> -->
+      <button @click="deletel">Delete</button>
       <button @click="addLocation">Ajout Location</button>
       <button @click="addWalk">Ajout Promenade</button>
-      <button @click="readLocation">Read Location</button>
+      <button @click="readAddress">Read Location</button>
       <button @click="readWalk">Read Promenade</button>
-      <button @click="updateLocation">Update Location</button>
+      <button @click="updateLocationCategory">Update Location</button>
       <button @click="updateWalk">Update Promenade</button>
     </div>
 </template>
@@ -24,30 +24,34 @@ export default {
   },
 
   firebase: {
-    documents: db.ref(),
-    locationsArr: db.ref('app/locations'), // loopable with v-for
-    locationsObj: { // can use keys, but v-for doesn't loop
-      source: db.ref('app/locations'),
-      asObject: true
-    },
-    walksArr: db.ref('app/walks'), // loopable with v-for
-    walksObj: { // can use keys, but v-for doesn't loop
-      source: db.ref('app/walks'),
-      asObject: true
-    }
+    documents: db.ref()
+    // locationsArr: db.ref('app/locations'), // loopable with v-for
+    // locationsObj: { // can use keys, but v-for doesn't loop
+    //   source: db.ref('app/locations'),
+    //   asObject: true
+    // },
+    // walksArr: db.ref('app/walks'), // loopable with v-for
+    // walksObj: { // can use keys, but v-for doesn't loop
+    //   source: db.ref('app/walks'),
+    //   asObject: true
+    // }
   },
   methods:{
-        addLocation(){
-          db.ref('app/locations').push({
-              name: 'Eglise Saint-Denis',
-              category: 'Culte',
-              address:'Place du Général Leclerc (Vieux Pontault)',
-              gps: '48.7826000, 2.6011282',
-              photos: {
-                main: '4 Eglise Saint-Denis.JPG',
-                photo2: '4-CPA.jpg'
-              }
-          })
+        addLocation(name, category, address, gps) {
+          var postData = {
+            name: 'Eglise Saint-Denis',
+            category: 'Culte',
+            address:'Place du Général Leclerc (Vieux Pontault)',
+            gps: '48.7826000, 2.6011282',
+            photos: {
+              main: '4 Eglise Saint-Denis.JPG',
+              photo2: '4-CPA.jpg'
+            }
+          };
+          // Write the new post's data simultaneously in the posts list and the user's post list.
+          var updates = {};
+          updates['Eglise Saint-Denis'] = postData;
+          db.ref('app/locations').update(updates);
         },
         addWalk(){
           db.ref('app/walks').push({
@@ -70,24 +74,41 @@ export default {
             console.log(documents)
           })
         },
-        updateLocation(){
-          for(location in locationsArr){
-             if(location.name=="")
-          }
-          db.ref(this.locationsObj[key]).equalTo('Eglise Saint-Denis').update({ gps: "hello" }).then(() => {
-              console.log('location updated!')
-          })
+        updateLocationCategory(){
+    
+          // db.ref('app/locations/Eglise Saint-Denis').once('value').then(function(snapshot) {
+          //   var name = (snapshot.val());
+          //   console.log(name.address)
+           
+          // });
+          var postData = {
+            category: 'Cultes',
+
+          };
+          // Write the new post's data simultaneously in the posts list and the user's post list.
+          var updates = {};
+          updates['Eglise Saint-Denis'] = postData;
+          db.ref('app/locations/Eglise Saint-Denis').update(postData);
+
+
         },
         updateWalk(){
           db.ref('app/walks').update({ id: "hello" }).then(() => {
               console.log('walk updated!')
           })
+        },
+        readAddress(){
+            db.ref('app/locations/Eglise Saint-Denis').once('value').then(function(snapshot) {
+            var name = (snapshot.val());
+            console.log(name.address)
+           
+          });
+        },
+        deletel(){
+            db.ref('app/locations/Eglise Saint-Denis').remove().then(() => {
+              console.log('Location delete!')
+          })
         }
-      // deletel(){
-      //     db.ref('documents/lieu/id').remove().then(() => {
-      //       console.log('titi delete!')
-      //   })
-      // }
   }
 }
 
