c5dc36ff9fcc5e9f66951958bac85e3b749d3bba
Sophie <sophbyr@gmail.com>
2020-01-24 12:04:07 +0100

    [+] Fix conflits

--- src/view/Balade.vue
+++ src/view/Balade.vue
@@ -19,17 +19,17 @@
                 </l-map>
             </div>
             <div class="baladeContainer">
-                <div class="placeTitle">{{ title }}</div>
+                <div class="placeTitle">{{ walk.name }}</div>
                 <div class="underPlaceTitle">
                     <div class="d-flex justify-content-between">
                         <div class="baladeDistance d-flex justify-content-center">
                             <div class="align-self-end"><img src="../img/distance-blue.svg" /></div>
-                            <div class="align-self-end km">{{ distance }}</div>
+                            <div class="align-self-end km">{{ walk.distance }}</div>
                         </div>
                         <div class="baladeDetails d-flex justify-content-center"><div class="align-self-end det">Détails</div></div>
                         <div class="baladeDuration d-flex justify-content-center">
                             <div class="align-self-end"><img src="../img/chronometer-blue.svg" /></div>
-                            <div class="align-self-end duration">{{ duration }}</div>
+                            <div class="align-self-end duration">{{ walk.duration }}</div>
                         </div>
                     </div>
                     <div class="text-center"><img class="downArrow" src="../img/down-arrow.svg" /></div>
@@ -41,7 +41,7 @@
             <div class="lieuxList">
                 <!-- mettre le lien du lieu dans le src suivant -->
                 <div>Sur le parcours :</div>
-                <a src="" @click="updatePage('Lieu', lieuBalade); setActiveLocation(lieuBalade)" v-for="lieuBalade in walk.locations">- {{ lieuBalade }}</a>
+                <a src="" @click="setActivePage('Lieu', lieuBalade); setActiveLocation(lieuBalade)" v-for="lieuBalade in walk.locations" v-bind:key="lieuBalade">- {{ lieuBalade }}</a>
             </div>
             <div class="placeBody">
                 <div class="placeText" v-html="walk.description"></div>
@@ -107,162 +107,160 @@
       BaladeFooter
     },
     methods: {
-         ... mapActions([
-                'setActiveLocation',
-                'setActivePage'
-          ]),
-      formated(coords) {
-        return latLng(coords)
-      },
-      increaseCenter() {
-        this.center = [this.center[0] + 0.0001, this.center[1] + 0.0001]
-        //console.log(this.center)
-      },  
-      trackPosition() {
-        if (navigator.geolocation) {
-          navigator.geolocation.watchPosition(this.successPosition, this.failurePosition, {enableHighAccuracy: true,
-              timeout: 15000,
-              maximumAge: 0,
-              })
-        } 
-        else {
-          alert(`Browser doesn't support Geolocation`)
-        }
-      },
-      successPosition: function(position) {
-        this.center = [position.coords.latitude, position.coords.longitude]
-        //console.log(this.center)
-      },
-      failurePosition: function(err) {
-        alert('Error Code: ' + err.code + ' Error Message: ' + err.message)
-      },
-      addMarkerLocation(){
-        let self=this
-        var query =  db.ref('app/locations/').orderByKey();
-        query.once("value")
-        .then(function(snapshot) {
-          snapshot.forEach(function(childSnapshot) {
-            var name = (childSnapshot.val());
-            let catIcon;
-            if (name.category == "Histoire"){
-              catIcon = 'https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color/254000/66-512.png'
+        ... mapActions([
+            'setActiveLocation',
+            'setActivePage'
+        ]),
+        formated(coords) {
+            return latLng(coords)
+        },
+        increaseCenter() {
+            this.center = [this.center[0] + 0.0001, this.center[1] + 0.0001]
+            //console.log(this.center)
+        },  
+        trackPosition() {
+            if (navigator.geolocation) {
+            navigator.geolocation.watchPosition(this.successPosition, this.failurePosition, {enableHighAccuracy: true,
+                timeout: 15000,
+                maximumAge: 0,
+                })
+            } 
+            else {
+            alert(`Browser doesn't support Geolocation`)
             }
-            if (name.category == "Culte"){
-              catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
-            }
-            if (name.category == "Nature"){
-              catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
-            }
-            if (name.category == "Culture"){
-              catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
-            }
-            if (name.category == "Parc"){
-              catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
-            }
-            let textContent = "<b>"+name.name+"</b>"+"<div><img style = 'height: 40px;' src='"+name.photos+"' alt='err'></div>"
-            if(name.gps) {
-                for (var i = 0; i < self.walk.locations.length; i++) {
-                    if (name.name == self.walk.locations[i]){
-                        self.markerList.push({coord: name.gps, text: textContent, category: catIcon})
+        },
+        successPosition: function(position) {
+            this.center = [position.coords.latitude, position.coords.longitude]
+            //console.log(this.center)
+        },
+        failurePosition: function(err) {
+            alert('Error Code: ' + err.code + ' Error Message: ' + err.message)
+        },
+        addMarkerLocation(){
+            let self=this
+            var query =  db.ref('app/locations/').orderByKey();
+            query.once("value").then(function(snapshot) {
+                snapshot.forEach(function(childSnapshot) {
+                    var name = (childSnapshot.val());
+                    let catIcon;
+                    if (name.category == "Histoire"){
+                    catIcon = 'https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color/254000/66-512.png'
                     }
-                }
-                this.$emit('updatePage', datas)
+                    if (name.category == "Culte"){
+                    catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
+                    }
+                    if (name.category == "Nature"){
+                    catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
+                    }
+                    if (name.category == "Culture"){
+                    catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
+                    }
+                    if (name.category == "Parc"){
+                    catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
+                    }
+                    let textContent = "<b>"+name.name+"</b>"+"<div><img style = 'height: 40px;' src='"+name.photos+"' alt='err'></div>"
+                    if(name.gps) {
+                        for (var i = 0; i < self.walk.locations.length; i++) {
+                            if (name.name == self.walk.locations[i]){
+                                self.markerList.push({coord: name.gps, text: textContent, category: catIcon})
+                            }
+                        }
+                        this.$emit('updatePage', datas)
 
-            },
-            formated(coords) {
-                return latLng(coords)
-            },
-            increaseCenter() {
-                this.center = [this.center[0] + 0.0001, this.center[1] + 0.0001]
-                //console.log(this.center)
-            },
-            trackPosition() {
-                if (navigator.geolocation) {
-                    navigator.geolocation.watchPosition(this.successPosition, this.failurePosition, {
-                        enableHighAccuracy: true,
-                        timeout: 15000,
-                        maximumAge: 0,
-                    })
-                }
-                else {
-                    alert(`Browser doesn't support Geolocation`)
-                }
-            },
-            successPosition: function (position) {
-                this.center = [position.coords.latitude, position.coords.longitude]
-                //console.log(this.center)
-            },
-            failurePosition: function (err) {
-                alert('Error Code: ' + err.code + ' Error Message: ' + err.message)
-            },
-            addMarkerLocation() {
-                let self = this
-                var query = db.ref('app/locations/').orderByKey();
-                query.once("value")
-                    .then(function (snapshot) {
-                        snapshot.forEach(function (childSnapshot) {
-                            var name = (childSnapshot.val());
-                            let catIcon;
-                            if (name.category == "Histoire") {
-                                catIcon = 'https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color/254000/66-512.png'
-                            }
-                            if (name.category == "Culte") {
-                                catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
-                            }
-                            if (name.category == "Nature") {
-                                catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
-                            }
-                            if (name.category == "Culture") {
-                                catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
-                            }
-                            if (name.category == "Parc") {
-                                catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
-                            }
-                            let textContent = "<div class='popupTitle'>" + name.name + "</div>" + "<div class='text-center'><img class='popupImage'  src='" + name.photos + "' alt='err'></div>"
-                            if (name.gps) {
-                                for (var i = 0; i < self.walk.locations.length; i++) {
-                                    if (name.name == self.walk.locations[i]) {
-                                        self.markerList.push({ coord: name.gps, text: textContent, category: catIcon })
-                                    }
-                                }
-                            }
-                        });
-                    });
-            },
-            addWalk() {
-                let self = this
-                var query = db.ref('app/walks/').orderByKey();
-                query.once("value")
-                    .then(function (snapshot) {
-                        snapshot.forEach(function (childSnapshot) {
-                            var name = (childSnapshot.val());
-                            if (name.name == self.walk.name) {
-                                self.polyline.latlngs.push(name.gps)
-                                if (name.category == "Histoire") {
-                                    self.polyline.color = "#ff66ff"
-                                }
-                                else if (name.category == "Culte") {
-                                    self.polyline.color = "#0099ff"
-                                }
-                                else if (name.category == "Nature") {
-                                    self.polyline.color = "#00ff99"
-                                }
-                                else if (name.category == "Culture") {
-                                    self.polyline.color = "#9900cc"
-                                }
-                                else if (name.category == "Parc") {
-                                    self.polyline.color = "#cc3300"
-                                }
-                                else {
-                                    self.polyline.color = "#000000"
-                                }
-                            }
-                        });
-                    });
+                    }
+                })
+            })
+        },
+        formated(coords) {
+            return latLng(coords)
+        },
+        increaseCenter() {
+            this.center = [this.center[0] + 0.0001, this.center[1] + 0.0001]
+            //console.log(this.center)
+        },
+        trackPosition() {
+            if (navigator.geolocation) {
+                navigator.geolocation.watchPosition(this.successPosition, this.failurePosition, {
+                    enableHighAccuracy: true,
+                    timeout: 15000,
+                    maximumAge: 0,
+                })
             }
-          });
-        });
-      }
+            else {
+                alert(`Browser doesn't support Geolocation`)
+            }
+        },
+        successPosition: function (position) {
+            this.center = [position.coords.latitude, position.coords.longitude]
+            //console.log(this.center)
+        },
+        failurePosition: function (err) {
+            alert('Error Code: ' + err.code + ' Error Message: ' + err.message)
+        },
+        addMarkerLocation() {
+            let self = this
+            var query = db.ref('app/locations/').orderByKey();
+            query.once("value")
+                .then(function (snapshot) {
+                    snapshot.forEach(function (childSnapshot) {
+                        var name = (childSnapshot.val());
+                        let catIcon;
+                        if (name.category == "Histoire") {
+                            catIcon = 'https://cdn1.iconfinder.com/data/icons/social-messaging-ui-color/254000/66-512.png'
+                        }
+                        if (name.category == "Culte") {
+                            catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
+                        }
+                        if (name.category == "Nature") {
+                            catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
+                        }
+                        if (name.category == "Culture") {
+                            catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
+                        }
+                        if (name.category == "Parc") {
+                            catIcon = 'http://simpleicon.com/wp-content/uploads/map-marker-2.png'
+                        }
+                        let textContent = "<div class='popupTitle'>" + name.name + "</div>" + "<div class='text-center'><img class='popupImage'  src='" + name.photos + "' alt='err'></div>"
+                        if (name.gps) {
+                            for (var i = 0; i < self.walk.locations.length; i++) {
+                                if (name.name == self.walk.locations[i]) {
+                                    self.markerList.push({ coord: name.gps, text: textContent, category: catIcon })
+                                }
+                            }
+                        }
+                    });
+                });
+        },
+        addWalk() {
+            let self = this
+            var query = db.ref('app/walks/').orderByKey();
+            query.once("value").then(function (snapshot) {
+                snapshot.forEach(function (childSnapshot) {
+                    var name = (childSnapshot.val());
+                    if (name.name == self.walk.name) {
+                        self.polyline.latlngs.push(name.gps)
+                        if (name.category == "Histoire") {
+                            self.polyline.color = "#ff66ff"
+                        }
+                        else if (name.category == "Culte") {
+                            self.polyline.color = "#0099ff"
+                        }
+                        else if (name.category == "Nature") {
+                            self.polyline.color = "#00ff99"
+                        }
+                        else if (name.category == "Culture") {
+                            self.polyline.color = "#9900cc"
+                        }
+                        else if (name.category == "Parc") {
+                            self.polyline.color = "#cc3300"
+                        }
+                        else {
+                            self.polyline.color = "#000000"
+                        }
+                    }
+                });
+            });
+        },
     },
     mounted: function() {
       this.trackPosition()
@@ -271,15 +269,14 @@
       this.walk=this.getActiveWalk
     },
     computed:{
-            ... mapGetters([
-                'getActivePage',
-                'getActiveWalk'
-            ]),
+        ... mapGetters([
+            'getActivePage',
+            'getActiveWalk'
+        ]),
     },
   };
 
 </script>
-
 
 
 <style>
