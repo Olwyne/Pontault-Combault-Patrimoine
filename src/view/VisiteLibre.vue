02583173a714023d6ddd7f65516ae3fd6bba3f2a
Mansion-Amelia <amelia.mansion@gmail.com>
2020-05-19 15:16:10 +0200

    test poco

--- src/view/VisiteLibre.vue
+++ src/view/VisiteLibre.vue
@@ -245,12 +245,10 @@
                   snapshot.forEach(function(childSnapshot) {
                     var name2 = (childSnapshot.val());
                     if (name.location == name2.name){
-                      if (self.measure(self.center[0], self.center[1], name2.gps[0], name2.gps[1]) < 10){
                         let catColor = "#435368";
                         var catIcon = 'https://firebasestorage.googleapis.com/v0/b/patrimoine-pontault-combault.appspot.com/o/app%2Fmarkers%2Fmarker-poco-export.svg?alt=media&token=6a49615e-0c9f-4999-bf66-f33933cb9a3c'                      
                         let textContent = "<div class='popupTitle' style='color:"+catColor+";'><b>"+name2.name+"</b></div>"+"<div class='text-center'><img class='popupImage' src='"+name2.photos+"' alt='err'></div>"
                         self.markerList.push({coord: name2.gps, text: textContent, category: catIcon, name:name2.name})
-                      }
                     }
                   });
                 });
